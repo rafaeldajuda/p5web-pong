e7f47e35a95dbdad42d25ca4cf811364012ae807
Rafael <rafaeldajuda@gmail.com>
2021-11-15 17:37:41 -0300

    configurando index.html

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+const paths = {
+    dev: "/home/rafael/Documentos/Alura/jogos/p5WEB/pong/",
+    heroku: "/app/"
+}
+
 //ARQUIVOS ESTATICOS
 app.use('/js', express.static('js'))
 
 app.listen(process.env.PORT || 3000, () => console.log("server start"));
 app.get('/', (req, res) => {
-    res.status(200).sendFile("/home/rafael/Documentos/Alura/jogos/p5WEB/pong/index.html")
+    res.status(200).sendFile(paths.heroku + "index.html");
 });

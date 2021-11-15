6476ed7b76cc97f5645068a4c4e0a39c09b0f8bf
Rafael <rafaeldajuda@gmail.com>
2021-11-15 17:34:38 -0300

    configurando porta

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 //ARQUIVOS ESTATICOS
 app.use('/js', express.static('js'))
 
-app.listen(3000, () => console.log("server port 3000 start"))
+app.listen(process.env.PORT || 3000, () => console.log("server start"));
 app.get('/', (req, res) => {
     res.status(200).sendFile("/home/rafael/Documentos/Alura/jogos/p5WEB/pong/index.html")
 });
